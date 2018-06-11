9a4bd25f39f1ed7bdff185508d80b3319f87c899
Celine Gauchey <celinegauchey@gmail.com>
2018-06-11 13:25:29 -0400

    2nd commit

--- bubblesort.js
+++ bubblesort.js
@@ -1,5 +1,18 @@
-function bubbleSort(array) {
-  return array
+function bubbleSort(arr) {
+  if (arr.length = 0){
+    return arr
+  }
+
+  for (var i = 0 j=i+1; i<arr.length; i++){
+    while (j < arr.length){
+      if (arr[i]>arr[j]){
+        arr[i] = arr[j]
+        arr[j] = arr[i]
+      }
+    }
+  }
+
+
   /* your code here */
 
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,13 +1,31 @@
 describe('Bubble Sort', function(){
+
+  const window = {};
+  window.swap = function () {
+    return 'swapped';
+  };
+  window.getSorted = function () {
+    this.swap();
+    return 'got sorted';
+  };
+
+  beforeAll(function () {
+    spyOn(window, 'swap').and.callThrough(); // replace existing `tootsiepop['lick']` method
+  });
+
+  it('gets the number of swaps', function () {
+    window.getSorted();
+    expect(window.swap.calls.count()).toEqual(arr.length);
+  });
+
   it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('sorts an array', function(){
     expect( bubbleSort([2,5,3,4,1,6,8,7]) ).toEqual( [1,2,3,4,5,6,7,8] );
   });
+
 });
 
-beforeAll(function () {
-  spyOn(window, 'swap').and.callThrough(); // replace existing `tootsiepop['lick']` method
-});
-it('getting to the center of tootsiepop involves exactly three licks', function () {
-  bubbleSort.getToCenter();
-  expect(bubbleSort.lick.calls.count()).toEqual(arr.length);
-});
+
